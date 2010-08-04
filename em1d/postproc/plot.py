626f6f38e1b1a3e1c7d33398bb67683d249f7045
Charles <charles@Ulysses.(none)>
2010-08-04 18:01:02 -0400

    Ajouté une barre de défilement à l'animation (plot.py).

--- em1d/postproc/plot.py
+++ em1d/postproc/plot.py
@@ -2,49 +2,79 @@
 # -*- coding: utf-8 -*-
 from pylab import *
 import gobject
+from sys import argv
+from matplotlib.widgets import Slider
 
+#########################################################################################
+# Get the data directory from the command line and fetch the data file names
+if size(argv) == 1: 
+  print "Please provide a data directory."
+  exit()
+else:
+  print "Data wil be read from %s" %argv[1]
+  def filename(n): return argv[1] + "out_%.06d.dat"%n
+
+#########################################################################################
+# Simulation parameters
 nsteps = 1000
-fileout = 10
+step = 10
 ncell = 400
 m2start  = int(ncell/2)
 
+#########################################################################################
+# The figure is created with frame 0
 fig = figure(figsize=(16.0,8.0))
 grid(True)
 xlabel('z')
 ylabel('Field')
-filename = "../output/out_%.06d.dat"%0
-d = loadtxt(filename)
+d = loadtxt(filename(0))
 curve1 = plot(d[:,0],d[:,1],label="Ex")
 curve2 = plot(d[:,0],d[:,2],label="Hy")
 legend()
 axvspan(m2start,ncell,facecolor='0.5', alpha=0.5)
 axis([0, ncell, -1.5, 1.5])
 
-n = 0
-def updatefig(*args):
-     global n
-     title('step %d' % n)
-     
-     #### Open files and read their content ###############
-     filename = "../output/out_%.06d.dat"%n
-     d = loadtxt(filename)
-     print filename
-     
-     #### Update data on graph ############################
-     curve1[0].set_ydata(d[:,1])
-     curve2[0].set_ydata(d[:,2])
-     
-     fig.canvas.draw()
-     
-     #### Jump to next step and check if done #############
-     n += fileout
-     if(n <= nsteps): return True
-     else: return False
-     
-     #### end of def ######################################
+#########################################################################################
+# Animation widgets
+axcolor = 'lightgoldenrodyellow'
+axdef  = axes([0.15, 0.02, 0.7, 0.02], axisbg=axcolor)
+progress = Slider(axdef,'', 0, nsteps, valinit=0)
 
-######### Update until updatefig returns false ############
-gobject.timeout_add(30,updatefig)
+resetax = axes([0.01, 0.02, 0.05, 0.1])
+playbutton = Button(resetax, '>||', color=axcolor, hovercolor='0.975')
+def play_pause(event):
+  global run
+  run = not run
+
+#########################################################################################
+# Function to update the figure
+def updatefig(*args):
+  global n
+
+  #######################################################################################
+  # Open file, read the content, and update data on the graph
+  d = loadtxt(filename(n))
+  curve1[0].set_ydata(d[:,1])
+  curve2[0].set_ydata(d[:,2])
+  fig.canvas.draw()
+
+  #######################################################################################
+  # Update n
+  playbutton.on_clicked(play_pause)
+  if(run): n += step
+  else:
+    val = int(progress.val)
+    n = val - val%10
+  if(n <= nsteps): 
+    progress.set_val(n)
+    return True
+  else: return False
+
+#########################################################################################
+# Do the animation
+n = 0
+run = True
+if(run): gobject.timeout_add(30,updatefig)
 show()
 
-######### End of script ###################################
+######### End of script #################################################################
