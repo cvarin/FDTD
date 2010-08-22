e4f27ced9fcb16814099d6c7086bf941150a42d6
Charles Varin <cvarin@gmail.com>
2010-08-22 16:33:24 -0400

    Revu et corrigé le modèle de l'oscillateur harmonique amorti.

--- dipmed/test_codes/damped_harmonic_oscillator.py
+++ dipmed/test_codes/damped_harmonic_oscillator.py
@@ -2,6 +2,14 @@
 # -*- coding: utf-8 -*-
 
 #########################################################################################
+# The equation for the damped and driven harmonic oscillator:
+# d^2 x/dT^2 + (gamma/omega_0) d x/dt + x = f(T)
+# where T = omega_0 t. It can be rewritten with two equations
+# d x/dt = v
+# d v/dt = f(T) - x - (gamma/omega_0) v
+# Ref. : http://en.wikipedia.org/wiki/Harmonic_oscillator
+
+#########################################################################################
 from numpy import *
 import pylab as p
 from scipy.integrate import odeint
@@ -11,9 +19,8 @@ from scipy.integrate import odeint
 tmax = 100
 numpoints = 300
 
-omega = 1.0          # Natural frequency
-b = 0.00001          # Damping factor
-x_0 = 0.0            # initials conditions
+b = 0.01         # Damping factor, effectively gamma/omega_0
+x_0 = 0.0        # initials conditions
 v_0 = 0.0
 X0 = array([x_0,v_0]) 
 
@@ -23,25 +30,30 @@ dt = float(tmax)/float(numpoints)
 #########################################################################################
 # Source
 def F_sine_gauss(t):
-     return exp(-(t-40.0)**2/200.0)*cos(t*omega/2.0)
+     return exp(-(t-40.0)**2/200.0)*cos(t/2.0)
+     
+def F_constant(t):
+     return 10.0;
 
 #########################################################################################
 F = F_sine_gauss
+#F = F_constant
 
 ################### Solution of the ode set #############################################
 def dX_dt(X,t=0):
-    return array([X[1], - b*X[1] -omega**2*F(t)])
+    return array([X[1], F(t) - X[0] - b*X[1]])
 X, infodict = odeint(dX_dt,X0,t,full_output=True)
 infodict['message']
 
 ################### The result is plotted ###############################################
 x,v = X.T
-time = t/omega
 fs=16
 p.figure(figsize=(16.0,8.5))
 p.title(r"Time evolution of a damped pendulum",fontsize=fs)
-p.plot(time,x/x.max(),'r-',label=r"Adaptive integrator")
-p.plot(time,F(time)/F(time).max(),'k',label="source")
+p.plot(t,x,'r-',label=r"Adaptive integrator")
+p.plot(t,F(t),'k',label="source")
+#p.plot(t,x/x.max(),'r-',label=r"Adaptive integrator")
+#p.plot(t,F(t)/F(t).max(),'k',label="source")
 p.xlabel(r"Time (in units of $\omega$)",fontsize=fs)
 p.ylabel(r"$x$",fontsize=1.5*fs)
 p.grid(True)
