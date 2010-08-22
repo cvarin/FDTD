1c18cae5be13924e46cfc1b779407c9329c3f956
Charles Varin <cvarin@gmail.com>
2010-08-22 15:32:41 -0400

    Solution de l'oscillateur harmonique amorti avec un intégrateur à pas adaptatif.

--- dipmed/test_codes/damped_harmonic_oscillator.py
+++ dipmed/test_codes/damped_harmonic_oscillator.py
@@ -7,45 +7,45 @@ import pylab as p
 from scipy.integrate import odeint
 
 #########################################################################################
-# Definition of the equations and parameters 
-Omega = 1.0              # Natural frequency
-b = 0.1                  # Damping factor
-theta_0 = pi*1.0/2.0     # Initial angle
-v_0     = 0.0            # Initial angular velocity
+# The equations of motion are integrated and the dynamics is plotted
+tmax = 100
+numpoints = 300
 
-def dX_dt(X, t=0):
-    """ Returns the variations of both the angle and velocity. """
-    return array([ X[1], - b*X[1] -Omega**2*sin(X[0])])
-    
+omega = 1.0          # Natural frequency
+b = 0.00001          # Damping factor
+x_0 = 0.0            # initials conditions
+v_0 = 0.0
+X0 = array([x_0,v_0]) 
+
+t = linspace(0,tmax,numpoints)
+dt = float(tmax)/float(numpoints)
+
 #########################################################################################
-# The pendulums equations of motion are integrated and the dynamics is plotted
-t = linspace(0, 100*Omega, 1000)           # time
-X0 = array([theta_0,v_0])                  # initials conditions
+# Source
+def F_sine_gauss(t):
+     return exp(-(t-40.0)**2/200.0)*cos(t*omega/2.0)
+
+#########################################################################################
+F = F_sine_gauss
 
 ################### Solution of the ode set #############################################
-X, infodict = odeint(dX_dt, X0, t, full_output=True)
-infodict['message']                     # >>> 'Integration successful.'
+def dX_dt(X,t=0):
+    return array([X[1], - b*X[1] -omega**2*F(t)])
+X, infodict = odeint(dX_dt,X0,t,full_output=True)
+infodict['message']
 
 ################### The result is plotted ###############################################
-theta, v = X.T
-time = t/Omega
+x,v = X.T
+time = t/omega
 fs=16
-f1 = p.figure()
-# Angle
-p.subplot(2,1,1)
+p.figure(figsize=(16.0,8.5))
 p.title(r"Time evolution of a damped pendulum",fontsize=fs)
-p.plot(time, theta, 'r-', label=r"$\theta$")
-p.plot(time, theta_0*exp(-b**1.3*t), 'g-', label=r"damping fit")
-p.ylabel(r"$\theta$ (rad)",fontsize=fs)
-p.grid()
+p.plot(time,x/x.max(),'r-',label=r"Adaptive integrator")
+p.plot(time,F(time)/F(time).max(),'k',label="source")
+p.xlabel(r"Time (in units of $\omega$)",fontsize=fs)
+p.ylabel(r"$x$",fontsize=1.5*fs)
+p.grid(True)
 p.legend(loc='best')
-
-# Velocity
-p.subplot(2,1,2)
-p.plot(time, v    , 'b-', label=r"$v$")
-p.grid()
-p.xlabel(r"Time (in units of $\Omega$)",fontsize=fs)
-p.ylabel(r"Angular velocity (rad/s)",fontsize=fs)
 
 #########################################################################################
 #p.savefig('fig.pdf')
